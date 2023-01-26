87fe06c2396dd569981f29d0e220e8893fc3e817
Dipak Kalekar <dipakkalekar44@gmail.com>
2023-01-27 00:24:31 +0530

    first commit for project setup

---
+++ src/main/java/com/castleart/CastleArtApplication.java
@@ -0,0 +1,13 @@
+package com.castleart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castleart/CastleArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castleart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
